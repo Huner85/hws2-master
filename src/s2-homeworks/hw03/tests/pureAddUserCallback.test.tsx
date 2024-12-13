fb6af05e9f2920d9655b8ba5f790833e63b8857b
Huner85 <hunter250185@gmail.com>
2024-12-13 13:38:12 +0300

    add HW3

--- src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import {pureAddUserCallback} from '../HW3'
+import React from 'react';
+import { pureAddUserCallback } from '../HW3';
+import { UserType } from '../HW3';
 
-let initialState: any[]
-const setName = (a: any[]) => {
-    initialState = a
-}
+let initialState: UserType[] = []; // Типизируем массив пользователей
+
+const mockSetUsers = (update: React.SetStateAction<UserType[]>): void => {
+    if (typeof update === 'function') {
+        // Если передана функция, вызываем её с текущим состоянием
+        initialState = (update as (prevState: UserType[]) => UserType[])(initialState);
+    } else {
+        // Если передано значение, устанавливаем его напрямую
+        initialState = update;
+    }
+};
 
 beforeEach(() => {
-    initialState = []
-})
+    initialState = []; // Очищаем перед каждым тестом
+});
 
 test('name 1', () => {
-    pureAddUserCallback('name', setName, initialState)
-    expect(initialState.length).toBe(1)
-    expect(initialState[0].name).toBe('name')
-    expect(!!initialState[0]._id).toBe(true)
-})
+    pureAddUserCallback('name', mockSetUsers, initialState); // Используем mockSetUsers
+    expect(initialState.length).toBe(1); // Проверяем, что пользователь добавлен
+    expect(initialState[0].name).toBe('name'); // Проверяем имя
+    expect(!!initialState[0]._id).toBe(true); // Проверяем наличие _id
+});
+
